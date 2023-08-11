30ccf8a054b14ac677c6ddba1e6a3b2585e47c19
Shinukis23 <juan.a.o.delgado@gmail.com>
2023-08-11 15:22:48 -0700

    Reporte de Eficiencia 1ra version

---
+++ Eficiencia.py
@@ -0,0 +1,195 @@
+# Programa para calcular la eficiencia de produccion de las tiendas
+# Modificado Agosto 9/2023 Se agrego Tienda de Ensenada
+
+from __future__ import print_function
+import pandas as pd
+import numpy as np
+import xlwt
+import openpyxl
+import xlsxwriter
+import gspread
+import pygsheets
+from oauth2client.service_account import ServiceAccountCredentials
+import json
+from datetime import timedelta
+from datetime import datetime as dt
+
+from Fun_EficienciaTest import *
+import gspread
+import sys
+import warnings
+import glob
+import os
+from gooey import Gooey, GooeyParser
+
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+scopes = [
+    'https://www.googleapis.com/auth/spreadsheets',
+    'https://www.googleapis.com/auth/drive'
+    ]
+
+credentials = ServiceAccountCredentials.from_json_keyfile_name("monitor-eficiencia-3a13458926a2.json", scopes) #access the json key you downloaded earlier 
+file = gspread.authorize(credentials)# authenticate the JSON key with gspread
+
+
+dfCortes = pd.read_csv(r'horasCorte.csv')
+H = dfCortes["horas"].tolist()
+M = dfCortes["minutos"].tolist()
+tiendas = ['1','2','6','7','8','4','14','10','15'] # Arreglo de numeros de tiendas Tapatio
+Jobs = []
+hora = 0
+JobStore = [[] for _ in range(len(tiendas))]
+columnas = [3,13,14,16]
+hojas = ['SiempreViva','Nirvana','San Bernardino','Rosamond','Rosamond','Desarme TJ','Desarme TJ','Economy','Ensenada']
+hojas2 = ['SiempreViva','Nirvana','San Bernardino','Rosamond','TDT','Economy','Ensenada']
+valor = ['Voided','New','Pickup']
+
+@Gooey(program_name="Eficiencia Diaria Tapatio Stores")
+def parse_args():
+    """ Use GooeyParser to build up the arguments we will use in our script
+    Save the arguments in a default json file so that we can retrieve them
+    every time we run the script.
+    """
+    stored_args = {}
+    # get the script name without the extension & use it to build up
+    # the json filename
+    script_name = os.path.splitext(os.path.basename(__file__))[0]
+    args_file = "{}-args.json".format(script_name)
+    # Read in the prior arguments as a dictionary
+    if os.path.isfile(args_file):
+        with open(args_file) as data_file:
+            stored_args = json.load(data_file)
+    parser = GooeyParser(description='Actualiza reporte diario des eficiencia de Google Sheets')
+    parser.add_argument('Archivo_CSV',
+                        action='store',
+                        default=stored_args.get('cust_file'),
+                        widget='FileChooser',
+                        help='Ej. JobsReport_20230317081108.csv')
+    parser.add_argument('Directorio_de_salida',
+                        action='store',
+                        default=stored_args.get('data_directory'),
+                        widget='DirChooser',
+                        help="Directorio de salida de arhivo .XLSX ")
+    parser.add_argument('Archivo_Corte',
+                        action='store',
+                        default=stored_args.get('cust_file'),
+                        widget='FileChooser',
+                        help='Ej. horaCortesFestivos.csv')
+    parser.add_argument('Fecha', help='Seleccione Fecha para generar reporte',
+                        default=stored_args.get('Fecha'),
+                        widget='DateChooser')
+    args = parser.parse_args()
+    # Store the values of the arguments so we have them next time we run
+    with open(args_file, 'w') as data_file:
+        # Using vars(args) returns the data as a dictionary
+        json.dump(vars(args), data_file)
+    return args
+
+
+def inicial():
+    scopes = [
+    'https://www.googleapis.com/auth/spreadsheets',
+    'https://www.googleapis.com/auth/drive'
+    ]
+
+    credentials = ServiceAccountCredentials.from_json_keyfile_name("monitor-eficiencia-3a13458926a2.json", scopes) #access the json key you downloaded earlier 
+    file = gspread.authorize(credentials)# authenticate the JSON key with gspread
+
+    columnas = [3,13,14,16]
+    tiendas = ['1','2','6','7','8','4','14','10','15'] # Arreglo de numeros de tiendas Tapatio
+    Jobs = []
+    JobStore = [[] for _ in range(len(tiendas))]
+    hojas = ['SiempreViva','Nirvana','San Bernardino','Rosamond','Rosamond','Desarme TJ','Desarme TJ','Economy','Ensenada']
+    hojas2 = ['SiempreViva','Nirvana','San Bernardino','Rosamond','TDT','Economy','Ensenada']
+    valor = ['Voided','New','Pickup']
+
+def Cortes(Archivo_Corte):
+    dfCortes = pd.read_csv(Archivo_Corte)
+    H = dfCortes["horas"].tolist()
+    M = dfCortes["minutos"].tolist()
+    return H,M
+
+#busca los trabajos de cada tienda de acuerdo a los horarios de corte
+def trabajos(store,i,cortes,date,ds2,datet):
+    cut = horaCortes(store,cortes,date)
+    Jobs.append(str(datetime.date(date)))
+    Jobs.append(hojas[i])
+    Jobs.append(len(ds2[(ds2['Part Store #'] == store) & (ds2['Created'] >= cut[0]) & (ds2['Created'] <= cut[1])] ))
+    Jobs.append(len(ds2[(ds2['Part Store #'] == store) & (ds2['Created'] >= cut[0]) & (ds2['Created'] <= cut[1]) & (ds2['Pulled Finished'] < datet)] ))
+    Jobs.append(len(ds2[(ds2['Part Store #'] == store) & (ds2['Created'] >= cut[0]) & (ds2['Created'] <= cut[1]) & (ds2['Job Status'] == 'Pulling Part')] ))
+    Jobs.append(len(ds2[(ds2['Part Store #'] == store) & (ds2['Created'] >= cut[0]) & (ds2['Created'] <= cut[1]) & (ds2['Job Status'] == 'Unassigned')] )) 
+    Jobs.append(len(ds2[(ds2['Part Store #'] == store) & (ds2['Created'] >= cut[1]) & (ds2['Created'] < datet)] ))
+    Jobs.append(len(ds2[(ds2['Part Store #'] == store) & (ds2['Created'] >= cut[1]) & (ds2['Created'] < datet) & (ds2['Pulled Finished'] < datet)] ))
+    JobStore[i].extend(Jobs)
+
+
+def sumaStores(JobStore,st1,st2):
+ for j in range(2,8):
+  JobStore[st1][j] = JobStore[st1][j] + JobStore[st2][j]
+ JobStore.pop(st2)
+  
+# Funcion Main para buscar todos los trabajos
+def principal(date,nombre,H,M,directorio):
+    date=dt.strptime(date, "%Y-%m-%d")
+    print(date)
+    ds = pd.read_csv(nombre)
+    head_tail = os.path.split(nombre)
+    nombre2 = head_tail[1].split('.')
+    nombre3 = directorio + "\\"+ nombre2[0] + ".xlsx"
+    nombre4 = directorio + "\\"+ nombre2[0] + "_Logistica.xlsx"
+    ds2 = timeFix(columnas,hora,ds)
+    rangoFechas(ds2,date)
+    print(ds2)
+    #writer = pd.ExcelWriter(nombre4, engine='xlsxwriter')
+    # Convert the dataframe to an XlsxWriter Excel object.
+    print("Creando archivo para Logistica", nombre4)
+    ds2.to_excel(nombre4,header=True, index = False)
+
+    print("Conectando a google sheets .....")
+    ss = file.open("EficienciaReporte")
+    print("Conexion exitosa")
+
+    borraColumnas('Job Status',valor,ds2)
+    #print(H[0])
+
+    cortes,datet = fechasCorte1(date,H,M)
+        
+    for i in range(len(tiendas)):
+     trabajos(int(tiendas[i]),i,cortes,date,ds2,datet)
+     Jobs.clear()
+
+    sumaStores(JobStore,3,4) 
+    sumaStores(JobStore,4,5)
+
+    # Actualizo reporte de eficiencia de Google Drive
+    for i in range(len(hojas2)):
+     hoja = ss.worksheet(str(hojas2[i]))
+     print(JobStore[i]) ####
+     hoja.append_row(JobStore[i])
+    print("Reporte de Eficiencia en Google Sheets actualizado correctamente") 
+    writer = pd.ExcelWriter(nombre3, engine='xlsxwriter')
+    # Convert the dataframe to an XlsxWriter Excel object.
+    print("Creando archivo", nombre3)
+    ds2.to_excel(writer, sheet_name='ProduccionDiaria',header=True, index = False)
+    
+
+    while True:
+        try:
+            writer.close()
+        except xlsxwriter.exceptions.FileCreateError as e:
+            decision = input("Exception caught in workbook.close(): %s\n"
+                             "Please close the file if it is open in Excel.\n"
+                             "Try to write file again? [Y/n]: " % e)
+            if decision != 'n':
+
+                continue
+
+        break
+
+if __name__ == '__main__':
+    conf = parse_args()
+    H,M=Cortes(conf.Archivo_Corte)
+    principal(conf.Fecha,conf.Archivo_CSV,H,M,conf.Directorio_de_salida)
+     
---
+++ Fun_EficienciaTest.py
@@ -0,0 +1,114 @@
+# Programa para calcular la eficiencia de produccion de las tiendas
+# Modificado Agosto 9/2023 Se agrego Tienda de Ensenada
+
+from datetime import datetime, timedelta
+import pandas as pd
+
+USAstores = [1,2,3,5,6,7,8]
+DesarmeTJ = [4,14]
+Economy = 10
+Ensenada = 15
+
+def fechasCorte1(date,H,M):
+ # Convierte argumento de entrada a fecha, y calculo fechas de cortes
+ datey = date - timedelta(days=1) # Ayer
+ datet = date + timedelta(days=1) # Mañana
+ dateS = date - timedelta(days=2) # Sabado
+ dateF = date - timedelta(days=3)
+ 
+ # Arreglo de fechas de cortes de tiendas Tapatio
+ cortes = [ datey + timedelta(hours = H[0])+ timedelta(minutes = M[0]),  #0 Stores USA stores
+            date + timedelta(hours = H[1])+ timedelta(minutes = M[1]),   #1 Stores USA stores
+            datey + timedelta(hours = H[2])+ timedelta(minutes = M[2]),  #2
+            date + timedelta(hours = H[3])+ timedelta(minutes = M[3]),   #3 Stores USA stores
+            datey + timedelta(hours = H[4])+ timedelta(minutes = M[4]), #4 Stores 4,14
+            date + timedelta(hours = H[5]) + timedelta(minutes= M[5]),  #5 Stores 4,14
+            datey + timedelta(hours = H[6])+ timedelta(minutes = M[6]),  #6 Store 10
+            date + timedelta(hours = H[7])+ timedelta(minutes = M[7]),   #7 Store 10
+            dateS + timedelta(hours = H[8])+ timedelta(minutes = M[8]),  #8 Stores USA stores
+            dateF + timedelta(hours = H[9])+ timedelta(minutes = M[9]), #9 Stores 4,14
+            dateS + timedelta(hours = H[10])+ timedelta(minutes = M[10]),  #10 Store 10
+            date + timedelta(hours = H[11]) + timedelta(minutes= M[11]),  #11 Stores 4,14  Sabado todo el dia en
+            datey + timedelta(hours = H[0])+ timedelta(minutes = M[0]),  #12 Tap 15 Ensenada
+            date + timedelta(hours = H[1])+ timedelta(minutes = M[1]),   #13 Tap 15 Enesenada
+            dateS + timedelta(hours = H[12])+ timedelta(minutes = M[12])  #14 Tap 15 Ensenada
+          ]
+ cortes = pd.to_datetime(cortes)
+ return cortes,datet
+
+def timeFix(col,h,ds):
+ # Sumo o resto horas para arreglar hora de reporte de Produccion
+ for i in range(len(col)):
+     n = int(col[i])
+     ds.iloc[:, n] = pd.to_datetime(ds.iloc[:,n])+ timedelta(hours=h)
+ ds3 = ds.copy()
+ return ds3
+
+def borraColumnas(col,value,ds2):
+    # Borro los registros que no se ocupan del reporte de produccion
+    for i in range(len(value)):
+     indexDeleted = ds2[ds2[col] == value[i]].index
+     ds2.drop(indexDeleted,inplace=True)
+    indexDeleted = ds2[ds2['Part Price'] <  0].index #borro regresos de dinero (retornos)
+    ds2.drop(indexDeleted,inplace=True)
+
+
+def rangoFechas(df,date):
+    #print(date)
+    #print(df['Created'].min())
+    #print(df['Created'].max())
+    if date <= df['Created'].min() or date >= df['Created'].max():
+        print("La fecha no se encuentra en el rango de fechas del archivo")
+        print("[",df['Created'].min()," - ", df['Created'].max(),"]")
+        exit()
+    
+# Selecciona los horarios de corte de acuerdo a la tienda    
+def horaCortes(store,cortes,date):
+    if date.weekday() == 0:  # (0 lunes)  
+        #print("Entre Semana")
+        if store in USAstores: #range(1,3) or store in range(6,9):
+            cuts = [cortes[8],cortes[1]]
+        elif store in DesarmeTJ:#(store == 14 or  store == 4):
+            cuts = [cortes[9],cortes[5]]
+        elif store == Economy:
+            cuts = [cortes[10],cortes[7]]
+        elif store == Ensenada:
+            cuts = [cortes[14],cortes[1]]    
+    elif date.weekday() == 5:      # 5 SABADO
+        #print("Sabado")
+        #print(store)
+        if store in USAstores: # range(1,3) or store in range(6,9):
+            cuts = [cortes[0],cortes[3]]
+        elif store in DesarmeTJ:#  (store == 14 or  store == 4):  
+            cuts = [cortes[4],cortes[11]]  
+        elif store == Economy:
+            cuts = [cortes[6],cortes[7]]
+        elif store == Ensenada:
+            cuts = [cortes[14],cortes[14]]    
+    else : #(1 martes) (2 miercoles) (3 jueves) (4 vienres) (5 sabado)
+        if store in USAstores:#range(1,3) or store in range(6,9):
+            cuts = [cortes[0],cortes[1]]
+        elif store in DesarmeTJ:#s(store == 14 or  store == 4):
+            cuts = [cortes[4],cortes[5]]
+        elif store == Economy:
+            cuts = [cortes[6],cortes[7]]
+        elif store == Ensenada:
+            cuts = [cortes[0],cortes[1]]    
+    return cuts
+
+#busca los trabajos de cada tienda de acuerdo a los horarios de corte
+def trabajos1(ds2,store,i,cortes,date,Jobs,JobStore,hojas,datet):
+    cut = horaCortes(store,cortes,date)
+    Jobs.append(str(datetime.date(date)))
+    Jobs.append(hojas[i])
+    Jobs.append(len(ds2[(ds2['Part Store #'] == store) & (ds2['Created'] >= cut[0]) & (ds2['Created'] <= cut[1])] ))
+    Jobs.append(len(ds2[(ds2['Part Store #'] == store) & (ds2['Created'] >= cut[0]) & (ds2['Created'] <= cut[1]) & (ds2['Pulled Finished'] < datet)] ))
+    Jobs.append(len(ds2[(ds2['Part Store #'] == store) & (ds2['Created'] >= cut[0]) & (ds2['Created'] <= cut[1]) & (ds2['Job Status'] == 'Pulling Part')] ))
+    Jobs.append(len(ds2[(ds2['Part Store #'] == store) & (ds2['Created'] >= cut[0]) & (ds2['Created'] <= cut[1]) & (ds2['Job Status'] == 'Unassigned')] )) 
+    Jobs.append(len(ds2[(ds2['Part Store #'] == store) & (ds2['Created'] >= cut[1]) & (ds2['Created'] < datet)] ))
+    Jobs.append(len(ds2[(ds2['Part Store #'] == store) & (ds2['Created'] >= cut[1]) & (ds2['Created'] < datet) & (ds2['Pulled Finished'] < datet)] ))
+    JobStore[i].extend(Jobs)
+
+
+
+
